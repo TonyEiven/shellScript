f8695ff12471145e9f125448033365f62901f779
tonyeiven <woaimayi123@163.com>
2017-11-14 01:06:44 +0800

    Have wrote a script

---
+++ macscan.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+from scapy.all import srp,Ether,ARP,conf
+ipscan="10.166.6.0/24"
+try:
+        ans,unans = srp(Ether(dst="FF:FF:FF:FF:FF:FF")/ARP(pdst=ipscan),timeout=2,verbose=False)
+except Exception,e:
+        print str(e)
+else:
+        for snd,rcv in ans:
+                list_mac = rcv.sprintf("%Ether.src% - %ARP.psrc%")
+                print list_mac
+
