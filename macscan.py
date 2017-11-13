61ae729651c5dbcd1215fcb126f59c9d13821914
tonyeiven <woaimayi123@163.com>
2017-11-14 03:14:21 +0800

    git template

--- macscan.py
+++ macscan.py
@@ -2,6 +2,7 @@
 
 # -*- coding: utf-8 -*-
 from scapy.all import srp,Ether,ARP,conf
+import sys,os
 ipscan="10.166.6.0/24"
 try:
         ans,unans = srp(Ether(dst="FF:FF:FF:FF:FF:FF")/ARP(pdst=ipscan),timeout=2,verbose=False)
@@ -11,4 +12,4 @@ else:
         for snd,rcv in ans:
                 list_mac = rcv.sprintf("%Ether.src% - %ARP.psrc%")
                 print list_mac
-
+print "This script could be used to scan survival host in lan zone"
